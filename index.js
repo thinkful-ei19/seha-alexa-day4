85c7c78a32db4db7c201be8108fe8e327f1adca7
Alexa Scott <arscott4792@gmail.com>
2018-02-22 11:47:13 -0800

    added to cat carousel problem

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 $(function() {
-    $('.thumbnail').click(function(event) {
-        
-    })
+    $('.thumbnail').on('click', function(event) {
+        const targetSrc = $(event.currentTarget).find('img').attr('src');
+        //prints src http
+        console.log(targetSrc);
 
-})
+        //prints alt
+        const targetAlt = $(event.currentTarget).find('img').attr('alt');
+        console.log(targetAlt);
 
-//.click OR .keydown OR .mousedown
+        //variable and log out
+
+        $('.hero').attr('src', targetSrc).attr('alt', targetAlt);
+    });
+
+});
+
+
+//.click OR .keydown OR .mousedown 
+//find image we clicked and replace with hero img -- src..
+//update alt
